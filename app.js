1afdcd5a60256b6c4deccd82c286252373551bc2
Sagar Sharma <tech.sagarsh@gmail.com>
2022-01-08 21:59:12 +0530

    Added more functionallity

--- app.js
+++ app.js
@@ -15,3 +15,34 @@ function slide(val){
     slideContainer.style.transform = `translate(0)`;
     }
 }
+
+// Testimonials ====================================================
+
+const TestimonialsSlide = document.querySelector('.testimonials');
+const testimonialItems = document.querySelectorAll('.testimonial');
+
+const lastTestimonialItem = testimonialItems[testimonialItems.length - 1];
+const firstTestimonialItem = testimonialItems[0];
+
+console.log(lastTestimonialItem.getBoundingClientRect())
+
+
+let item = 500; 
+let item2 = 250;
+
+function makeSwipe(arg) {
+
+    if(arg === 1){
+    if(lastTestimonialItem.getBoundingClientRect().left < 1000) return;
+    TestimonialsSlide.style.transform = `translate(-${item}px)`;
+    item += 500;
+    item2-= 250;
+    }
+    
+    else{
+    if(firstTestimonialItem.getBoundingClientRect().right > (window.innerWidth / 2)) return;
+    item -= 500;
+    TestimonialsSlide.style.transform = `translate(${item2}px)`;
+    item2 += 250;
+    }
+}
